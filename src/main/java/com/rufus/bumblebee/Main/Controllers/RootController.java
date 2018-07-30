8ba770ccd84a5cf7d3552b20599ea72dcf03da24
TimurBaldin <qazxswsxz111@gmail.com>
2018-07-30 12:37:41 +0400

    Fix index.html and RootController

--- src/main/java/com/rufus/bumblebee/Main/Controllers/RootController.java
+++ src/main/java/com/rufus/bumblebee/Main/Controllers/RootController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class RootController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = { "/", "/index" })
     public String start() {
         return "index";
     }
