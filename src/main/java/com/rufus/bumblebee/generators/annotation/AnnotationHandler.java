8a7f1c4365c0ef9022ae10bd9a2bfd0413330067
Timur Baldin <qazxswsxz111@gmail.com>
2021-09-02 20:13:43 +0300

    поправил обработку аннотаций и конфиги

--- src/main/java/com/rufus/bumblebee/generators/annotation/AnnotationHandler.java
+++ src/main/java/com/rufus/bumblebee/generators/annotation/AnnotationHandler.java
@@ -1,13 +1,15 @@
 package com.rufus.bumblebee.generators.annotation;
 
 import com.rufus.bumblebee.generators.BaseGenerator;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,8 +19,9 @@ import java.util.Map;
 @Component
 public class AnnotationHandler {
 
-    private static final Map<GeneratorDescription, List<GeneratorParameter>>
-            generatorBeans = new HashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(AnnotationHandler.class);
+
+    private static final Map<GeneratorDescription, List<GeneratorParameter>> generatorBeans = new HashMap<>();
 
     private final List<BaseGenerator> generators;
 
@@ -73,21 +76,30 @@ public class AnnotationHandler {
         return new Object();
     }
 
-    public void setParameters(Field[] fields, Map<String, String> values, BaseGenerator generator) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public void setParameters(Field[] fields, Map<String, String> values, BaseGenerator generator) throws Exception {
         for (Field field : fields) {
             GeneratorParameter annotation = field.getAnnotation(GeneratorParameter.class);
             if (annotation != null) {
                 String value = values.get(annotation.name());
                 if (value == null) {
-                    return;
-                }
-                if (!annotation.InClass().equals(String.class)) {
-                    Method method = annotation.InClass().getMethod("valueOf", String.class);
-                    field.set(generator, method.invoke(annotation.InClass(), value));
-                } else {
-                    field.set(generator, value);
+                    log.error("For one of the parameters of the {} generator, a name is not specified in the annotation GeneratorParameter", generator.getGeneratorName());
+                    throw new Exception("The parameter name is not specified in the GeneratorParameter annotation for the generator: " + generator.getGeneratorName());
                 }
 
+                if (StringUtils.isNotEmpty(annotation.convertMethod())) {
+                    Method method = annotation.InClass().getMethod(annotation.convertMethod(), annotation.InClass());
+                    field.set(generator, method.invoke(annotation.InClass(), value));
+                } else {
+                    if (!annotation.InClass().equals(String.class)) {
+                        Method method = annotation.InClass().getMethod("valueOf", String.class);
+                        field.set(generator, method.invoke(annotation.InClass(), value));
+                    } else {
+                        field.set(generator, value);
+                    }
+                }
+            } else {
+                log.error("An annotation GeneratorParameter was not found for one of the fields of the generator {}", generator.getGeneratorName());
+                throw new Exception("For one of the parameters , the annotation GeneratorParameter was not found , for the generator: " + generator.getGeneratorName());
             }
         }
     }
